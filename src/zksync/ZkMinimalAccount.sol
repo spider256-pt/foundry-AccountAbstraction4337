af0d1c77c3b17af832328277bd0bf39333e58350
spider256-pt <daspra0tik@gmail.com>
2026-07-11 12:01:03 +0530

    Completed: zksync's Minimal Contract will write test soon

--- src/zksync/ZkMinimalAccount.sol
+++ src/zksync/ZkMinimalAccount.sol
@@ -17,12 +17,17 @@ import {
 
 import {
     NONCE_HOLDER_SYSTEM_CONTRACT,
-    BOOTLOADER_FORMAL_ADDRESS
+    BOOTLOADER_FORMAL_ADDRESS,
+    DEPLOYER_SYSTEM_CONTRACT
 } from "lib/foundry-era-contracts/src/system-contracts/contracts/Constants.sol";
 
 import {
     INonceHolder
 } from "lib/foundry-era-contracts/src/system-contracts/contracts/interfaces/INonceHolder.sol";
+
+import {
+    Utils
+} from "lib/foundry-era-contracts/src/system-contracts/contracts/libraries/Utils.sol";
 
 import {ECDSA} from "@openzeppelin/contracts/utils/cryptography/ECDSA.sol";
 import {
@@ -40,12 +45,31 @@ contract ZkMinimalAccount is IAccount, Ownable {
 
     constructor() Ownable(msg.sender) {}
 
+    /*//////////////////////////////////////////////////////////////
+                                 ERROR
+    //////////////////////////////////////////////////////////////*/
+
     error zkSyncAccount__NotEnoughBalance();
     error zkMinimalAccount__NotFromBootloader();
+    error zkMinimalAccount__ExecutionFailed();
+    error zkMinimalAccount__NotFromBootloaderorOwner();
+    error zkMinimalAccount__FailedToPay();
+    error zkMinimalAccount__InvalidSignature();
+
+    /*//////////////////////////////////////////////////////////////
+                                MODIFIER
+    //////////////////////////////////////////////////////////////*/
 
     modifier requireFromBootloader() {
         if (msg.sender != BOOTLOADER_FORMAL_ADDRESS) {
             revert zkMinimalAccount__NotFromBootloader();
+        }
+        _;
+    }
+
+    modifier requireFromBootLoaderOrOwner() {
+        if (msg.sender != BOOTLOADER_FORMAL_ADDRESS && msg.sender != owner()) {
+            revert zkMinimalAccount__NotFromBootloaderorOwner();
         }
         _;
     }
@@ -74,10 +98,59 @@ contract ZkMinimalAccount is IAccount, Ownable {
 
      */
     function validateTransaction(
-        bytes32 _txHash,
-        bytes32 _suggestedSignedHash,
+        bytes32, //_txHash,
+        bytes32, //_suggestedSignedHash,
         Transaction calldata _transaction
     ) external payable override requireFromBootloader returns (bytes4 magic) {
+        return _validateTransaction(_transaction);
+    }
+
+    function executeTransaction(
+        bytes32, //_txHash,
+        bytes32, //_suggestedSignedHash, //Not that neccessary for now as we are not using paymaster.
+        Transaction calldata _transaction
+    ) external payable override requireFromBootLoaderOrOwner {
+        _executeTransaction(_transaction);
+    }
+
+    function executeTransactionFromOutside(
+        Transaction calldata _transaction
+    ) external payable override {
+        bytes4 magic = _validateTransaction(_transaction);
+        if (magic != ACCOUNT_VALIDATION_SUCCESS_MAGIC) {
+            revert zkMinimalAccount__InvalidSignature();
+        }
+        _executeTransaction(_transaction);
+    }
+
+    function payForTransaction(
+        bytes32 /*_txHash*/,
+        bytes32 /*_suggestedSignedHash*/,
+        Transaction calldata _transaction
+    ) external payable override {
+        bool success = _transaction.payToTheBootloader();
+        if (!success) {
+            revert zkMinimalAccount__FailedToPay();
+        }
+    }
+
+    function prepareForPaymaster(
+        bytes32 _txHash,
+        bytes32 _possibleSignedHash,
+        Transaction calldata _transaction
+    ) external payable override {
+        //Not needed for this as there is no PayMaster functionality in it.
+    }
+
+    receive() external payable {}
+
+    /*//////////////////////////////////////////////////////////////
+                           INTERNAL FUNCTION
+    //////////////////////////////////////////////////////////////*/
+
+    function _validateTransaction(
+        Transaction memory _transaction
+    ) internal returns (bytes4 magic) {
         //Validartes the nonce for prventing replay attacks.
         SystemContractsCaller.systemCallWithPropagatedRevert(
             uint32(gasleft()),
@@ -107,36 +180,40 @@ contract ZkMinimalAccount is IAccount, Ownable {
         return magic;
     }
 
-    function executeTransaction(
-        bytes32 _txHash,
-        bytes32 _suggestedSignedHash,
-        Transaction calldata _transaction
-    ) external payable override {
-        //logic
-    }
-    function executeTransactionFromOutside(
-        Transaction calldata _transaction
-    ) external payable override {
-        //logic
-    }
+    function _executeTransaction(Transaction memory _transaction) internal {
+        //Gets the target address.
+        address to = address(uint160(_transaction.to));
+        //Gets the value to be sent with the transaction.
+        /*Type casted because EVM or any other contract may use uint128 so type cast is neccessary as it avoids the
+        problem of overflow.*/
+        uint128 value = Utils.safeCastToU128(_transaction.value);
+        //Gets the calldata to be sent with the transaction.
+        bytes memory data = _transaction.data;
 
-    function payForTransaction(
-        bytes32 _txHash,
-        bytes32 _suggestedSignedHash,
-        Transaction calldata _transaction
-    ) external payable override {
-        //logic
+        if (to == address(DEPLOYER_SYSTEM_CONTRACT)) {
+            uint32 gas = Utils.safeCastToU32(gasleft());
+            SystemContractsCaller.systemCallWithPropagatedRevert(
+                gas,
+                to,
+                value,
+                data
+            );
+        } else {
+            bool success;
+            assembly {
+                success := call(
+                    gas(), //gas for the execution
+                    to, //target address
+                    value, //amount to send
+                    add(data, 0x20), //pointer to actual calldata as it starts for prifix 0x20
+                    mload(data), //length of the call data
+                    0, //return pointer
+                    0 //size of the return data
+                )
+            }
+            if (!success) {
+                revert zkMinimalAccount__ExecutionFailed();
+            }
+        }
     }
-
-    function prepareForPaymaster(
-        bytes32 _txHash,
-        bytes32 _possibleSignedHash,
-        Transaction calldata _transaction
-    ) external payable override {
-        //logic
-    }
-
-    /*//////////////////////////////////////////////////////////////
-                           INTERNAL FUNCTION
-    //////////////////////////////////////////////////////////////*/
 }
